598e188c919cfad4eb3735c3765599775a965851
Renaud Cizeron <renaud.cizeron@arolla.fr>
2022-02-01 13:14:26 +0100

    US1: non-empty Initial Deposit is ok

--- src/main/java/fr/arolla/sgkata/Account.java
+++ src/main/java/fr/arolla/sgkata/Account.java
@@ -4,10 +4,18 @@ import java.math.BigDecimal;
 
 public class Account {
 
+
+    private final BigDecimal initialDeposit;
+
     public Account() {
+        this.initialDeposit = BigDecimal.ZERO;
+    }
+
+    public Account(BigDecimal initialDeposit) {
+        this.initialDeposit = initialDeposit;
     }
 
     public BigDecimal getBalance() {
-        return BigDecimal.ZERO;
+        return this.initialDeposit;
     }
 }
