d288deead4dfb218f61c8937dd3ec0bd7b1a10d2
Renaud Cizeron <renaud.cizeron@arolla.fr>
2022-02-01 16:59:47 +0100

    US3 : Several deposits and withdrawals are OK

--- src/test/java/fr/arolla/sgkata/HistoryTest.java
+++ src/test/java/fr/arolla/sgkata/HistoryTest.java
@@ -30,6 +30,22 @@ public class HistoryTest {
                 new Statement(Transaction.Type.DEPOSIT, LocalDate.of(2022, 2, 2), BigDecimal.valueOf(150), BigDecimal.valueOf(250)),
                 new Statement(Transaction.Type.DEPOSIT, LocalDate.of(2022, 2, 3), BigDecimal.valueOf(200), BigDecimal.valueOf(450))
         );
+    }
 
+    @Test
+    void several_deposits_and_withdrawals() {
+        Account account = new Account(BigDecimal.valueOf(100), LocalDate.of(2022, 2, 1));
+        account.depose(BigDecimal.valueOf(150), LocalDate.of(2022, 2, 2));
+        account.withdraw(BigDecimal.valueOf(200), LocalDate.of(2022, 2, 3));
+        account.depose(BigDecimal.valueOf(250), LocalDate.of(2022, 2, 4));
+        account.withdraw(BigDecimal.valueOf(300), LocalDate.of(2022, 2, 5));
+
+        assertThat(Statement.getStatements(account)).containsExactly(
+                new Statement(Transaction.Type.DEPOSIT, LocalDate.of(2022, 2, 1), BigDecimal.valueOf(100), BigDecimal.valueOf(100)),
+                new Statement(Transaction.Type.DEPOSIT, LocalDate.of(2022, 2, 2), BigDecimal.valueOf(150), BigDecimal.valueOf(250)),
+                new Statement(Transaction.Type.WITHDRAWAL, LocalDate.of(2022, 2, 3), BigDecimal.valueOf(200), BigDecimal.valueOf(50)),
+                new Statement(Transaction.Type.DEPOSIT, LocalDate.of(2022, 2, 4), BigDecimal.valueOf(250), BigDecimal.valueOf(300)),
+                new Statement(Transaction.Type.WITHDRAWAL, LocalDate.of(2022, 2, 5), BigDecimal.valueOf(300), BigDecimal.valueOf(0))
+        );
     }
 }
